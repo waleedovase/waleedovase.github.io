e675db0be12cbdd5c4102332e051428e0d3ec602
waleedovase <waleed.ovase@nyu.edu>
2016-05-28 01:21:36 -0700

    Update code.js

--- code.js
+++ code.js
@@ -56,9 +56,9 @@ $('#cy').cytoscape({
       { data: { id: '7', name: 'This is the bridge, were going to need you to get up here', weight: 80, faveColor: '#e30915', faveShape: 'triangle' } },
       { data: { id: '8', name: 'Captain, weve lost Admiral Jenkins, youre the highest ranking officer now', weight: 80, faveColor: '#e30915', faveShape: 'triangle' } },
       { data: { id: '9', name: 'Im the highest ranking? Jeezuz, was everyone in the dining room?', weight: 80, faveColor: '#6FB1FC', faveShape: 'octagon' } },
-      { data: { id: '10', name: 'Yes sir, you are.  Were going to need you to come up to the bridge.', weight: 80, faveColor: '#e30915', faveShape: 'triangle' } }
-    //  { data: { id: '11', name: 'Is Dr. Cholodenko still alive?', weight: 80, faveColor: '#6FB1FC', faveShape: 'octagon' } },
-    //  { data: { id: '12', name: 'From Xenobiology?', weight: 80, faveColor: '#e30915', faveShape: 'triangle' } },
+      { data: { id: '10', name: 'Yes sir, you are.  Were going to need you to come up to the bridge.', weight: 80, faveColor: '#e30915', faveShape: 'triangle' } },
+      { data: { id: '11', name: 'Is Dr. Cholodenko still alive?', weight: 80, faveColor: '#6FB1FC', faveShape: 'octagon' } },
+      { data: { id: '12', name: 'From Xenobiology?', weight: 80, faveColor: '#e30915', faveShape: 'triangle' } }
     //  { data: { id: '13', name: 'I can...check that. But, the computers. Most everything is blown or offline! Cant seem to get a response from engineering.', weight: 80, faveColor: '#e30915', faveShape: 'triangle'} },
     //  { data: { id: '14', name: 'Yes, from xenobiology. We need to make sure shes still alive.', weight: 80, faveColor: '#6FB1FC', faveShape: 'octagon' } },
     //  { data: { id: '15', name: 'Ill stay on the bridge, you should head towards xenobiology.', weight: 80, faveColor: '#e30915', faveShape: 'triangle' } },
