638a7d5e0a85f8269b1c5bbfcddc4328de87af53
waleedovase <waleed.ovase@nyu.edu>
2016-05-28 01:46:18 -0700

    Update code.js

--- code.js
+++ code.js
@@ -2,8 +2,23 @@ $(function(){ // on dom ready
 
 $('#cy').cytoscape({
   layout: {
-    name: 'cose',
-    padding: 10
+    name: 'grid',
+
+  fit: true, // whether to fit the viewport to the graph
+  padding: 30, // padding used on fit
+  boundingBox: undefined, // constrain layout bounds; { x1, y1, x2, y2 } or { x1, y1, w, h }
+  avoidOverlap: true, // prevents node overlap, may overflow boundingBox if not enough space
+  avoidOverlapPadding: 10, // extra spacing around nodes when avoidOverlap: true
+  condense: false, // uses all available space on false, uses minimal space on true
+  rows: undefined, // force num of rows in the grid
+  cols: undefined, // force num of columns in the grid
+  position: function( node ){}, // returns { row, col } for element
+  sort: undefined, // a sorting function to order the nodes; e.g. function(a, b){ return a.data('weight') - b.data('weight') }
+  animate: false, // whether to transition the node positions
+  animationDuration: 500, // duration of animation in ms if enabled
+  animationEasing: undefined, // easing of animation if enabled
+  ready: undefined, // callback on layoutready
+  stop: undefined // callback on layoutstop
   },
   
   style: cytoscape.stylesheet()
