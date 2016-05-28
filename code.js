489d3eaf625ddcca92690623584c4e8ab8698009
waleedovase <waleed.ovase@nyu.edu>
2016-05-28 01:47:46 -0700

    Update code.js

--- code.js
+++ code.js
@@ -2,23 +2,30 @@ $(function(){ // on dom ready
 
 $('#cy').cytoscape({
   layout: {
-    name: 'grid',
-
+    name: 'concentric',
   fit: true, // whether to fit the viewport to the graph
-  padding: 30, // padding used on fit
+  padding: 30, // the padding on fit
+  startAngle: 3/2 * Math.PI, // where nodes start in radians
+  sweep: undefined, // how many radians should be between the first and last node (defaults to full circle)
+  clockwise: true, // whether the layout should go clockwise (true) or counterclockwise/anticlockwise (false)
+  equidistant: false, // whether levels have an equal radial distance betwen them, may cause bounding box overflow
+  minNodeSpacing: 10, // min spacing between outside of nodes (used for radius adjustment)
   boundingBox: undefined, // constrain layout bounds; { x1, y1, x2, y2 } or { x1, y1, w, h }
   avoidOverlap: true, // prevents node overlap, may overflow boundingBox if not enough space
-  avoidOverlapPadding: 10, // extra spacing around nodes when avoidOverlap: true
-  condense: false, // uses all available space on false, uses minimal space on true
-  rows: undefined, // force num of rows in the grid
-  cols: undefined, // force num of columns in the grid
-  position: function( node ){}, // returns { row, col } for element
-  sort: undefined, // a sorting function to order the nodes; e.g. function(a, b){ return a.data('weight') - b.data('weight') }
+  height: undefined, // height of layout area (overrides container height)
+  width: undefined, // width of layout area (overrides container width)
+  concentric: function(node){ // returns numeric value for each node, placing higher nodes in levels towards the centre
+  return node.degree();
+  },
+  levelWidth: function(nodes){ // the variation of concentric values in each level
+  return nodes.maxDegree() / 4;
+  },
   animate: false, // whether to transition the node positions
   animationDuration: 500, // duration of animation in ms if enabled
   animationEasing: undefined, // easing of animation if enabled
   ready: undefined, // callback on layoutready
   stop: undefined // callback on layoutstop
+
   },
   
   style: cytoscape.stylesheet()
